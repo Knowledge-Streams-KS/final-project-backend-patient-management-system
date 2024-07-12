30f2aee568ac8d094b4a6effed92f70f0989e365
Maryam Saba <maryams91101@gmail.com>
2024-07-13 01:30:44 +0500

    cors error fixed

--- src/app.js
+++ src/app.js
@@ -1,31 +1,36 @@
 import 'dotenv/config';
-import express from 'express'
+import express from 'express';
 import { connectDB } from './db/config.js';
 import syncDB from './db/init.js';
 import AllRoutes from './Routes/index.js';
-//import bodyParser from 'body-parser';
-import cors from "cors"
-const myApp = express();
-const port = process.env.PORT
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-myApp.use(express.json())
-//myApp.use(bodyParser.urlencoded({ extended: true }));
-myApp.use(AllRoutes);
+const myApp = express();
+const port = process.env.PORT;
+
 // Enable CORS
 myApp.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   credentials: true,
-  //allowedHeaders: ['Content-Type']
+  allowedHeaders: 'Content-Type'
 }));
+//you must enable cors before routing or u'll face CORS errors!
+
+myApp.use(express.json());
+myApp.use(bodyParser.urlencoded({ extended: true }));
+
+myApp.use(AllRoutes);
+
 async function main() {
   await connectDB();
-  console.log("DB is connected")
+  console.log("DB is connected");
   await syncDB();
-  console.log("DB is synced")
-  myApp.listen(port,()=> {
-    console.log(`server is running at port ${port}`)
-  })
+  console.log("DB is synced");
+  myApp.listen(port, () => {
+    console.log(`server is running at port ${port}`);
+  });
 }
 
 main().catch((error) => {
